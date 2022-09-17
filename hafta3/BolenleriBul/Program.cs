1b0d270a9f5b51483108d8dc3bd2b8894394ceed
atahan <39523757+AtahanCelebi@users.noreply.github.com>
2022-09-17 11:48:59 +0300

    Add files via upload

---
+++ hafta3/BolenleriBul/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolenleriBul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList x = bolenleriBul(32);
+            //Console.WriteLine(x);
+
+
+            Console.Write("super sayımı: ");
+            int sayi = Int32.Parse(Console.ReadLine());
+            Console.Write(superSayimi(sayi));
+            //superSayimi(28) ? Console.WriteLine(sayi + " super sayidir") : Console.WriteLine(sayi + " super sayi değildir");
+
+
+            Console.ReadKey();
+        }
+        static ArrayList bolenleriBul(int myNum)
+        {
+            ArrayList bolenler = new ArrayList();
+
+            for(int i = 1; i < myNum; i++)
+            {
+                if (myNum % i == 0)
+                {
+                    Console.WriteLine(i + " ile bölünüyor");
+                    bolenler.Add(i);
+                }
+            }
+
+            return bolenler;
+
+        }
+
+        static bool superSayimi(int isSuper)
+        {
+            ArrayList bolenlerim = bolenleriBul(isSuper);
+            int toplam = 0;
+            foreach(int item in bolenlerim)
+            {
+                toplam += item;
+            }
+
+            return toplam == isSuper;
+
+        }
+    }
+}
---
+++ hafta3/tersCevir/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tersCevir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = tersCevir("ali");
+            Console.WriteLine(x);
+
+            Console.Write("metin giriniz: ");
+            string metin = Console.ReadLine();
+            Console.WriteLine(tersCevir(metin));
+            Console.ReadKey();
+
+
+        }
+        static string tersCevir(string myStr)
+        {
+            string result = "";
+            int strLength = myStr.Length;
+            for(int i = strLength-1; i >= 0; i--)
+            {
+                result += myStr[i];
+            }
+
+            return result;
+        }
+    }
+}
