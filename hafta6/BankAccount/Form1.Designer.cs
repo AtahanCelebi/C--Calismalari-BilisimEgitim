df123623f82a83a9a7232de0c78f16a759c59cd3
atahan <39523757+AtahanCelebi@users.noreply.github.com>
2022-10-14 20:23:31 +0300

    bankaccount-hw

---
+++ hafta6/BankAccount/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace BankAccount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtTutar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnParaYatir = new System.Windows.Forms.Button();
+            this.btnParaCek = new System.Windows.Forms.Button();
+            this.txtBakiye = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridBakiye = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TL = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLimit = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridBakiye)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTutar
+            // 
+            this.txtTutar.Location = new System.Drawing.Point(82, 46);
+            this.txtTutar.Name = "txtTutar";
+            this.txtTutar.Size = new System.Drawing.Size(117, 20);
+            this.txtTutar.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tutar Giriniz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(205, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "TL";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnParaYatir
+            // 
+            this.btnParaYatir.Location = new System.Drawing.Point(12, 87);
+            this.btnParaYatir.Name = "btnParaYatir";
+            this.btnParaYatir.Size = new System.Drawing.Size(128, 43);
+            this.btnParaYatir.TabIndex = 3;
+            this.btnParaYatir.Text = "Para Yatır";
+            this.btnParaYatir.UseVisualStyleBackColor = true;
+            this.btnParaYatir.Click += new System.EventHandler(this.btnParaYatir_Click);
+            // 
+            // btnParaCek
+            // 
+            this.btnParaCek.Location = new System.Drawing.Point(146, 87);
+            this.btnParaCek.Name = "btnParaCek";
+            this.btnParaCek.Size = new System.Drawing.Size(128, 43);
+            this.btnParaCek.TabIndex = 4;
+            this.btnParaCek.Text = "Para Çek";
+            this.btnParaCek.UseVisualStyleBackColor = true;
+            this.btnParaCek.Click += new System.EventHandler(this.btnParaCek_Click);
+            // 
+            // txtBakiye
+            // 
+            this.txtBakiye.AutoSize = true;
+            this.txtBakiye.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBakiye.Location = new System.Drawing.Point(358, 28);
+            this.txtBakiye.Name = "txtBakiye";
+            this.txtBakiye.Size = new System.Drawing.Size(15, 15);
+            this.txtBakiye.TabIndex = 5;
+            this.txtBakiye.Text = "0";
+            this.txtBakiye.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(269, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "BAKİYENİZ:";
+            // 
+            // dataGridBakiye
+            // 
+            this.dataGridBakiye.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridBakiye.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridBakiye.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridBakiye.Location = new System.Drawing.Point(0, 209);
+            this.dataGridBakiye.Name = "dataGridBakiye";
+            this.dataGridBakiye.Size = new System.Drawing.Size(442, 237);
+            this.dataGridBakiye.TabIndex = 7;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "İslem Türü";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Miktar";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Güncel Bakiye";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "İslem Tarihi";
+            this.Column4.Name = "Column4";
+            // 
+            // TL
+            // 
+            this.TL.AutoSize = true;
+            this.TL.Location = new System.Drawing.Point(399, 30);
+            this.TL.Name = "TL";
+            this.TL.Size = new System.Drawing.Size(20, 13);
+            this.TL.TabIndex = 8;
+            this.TL.Text = "TL";
+            this.TL.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(312, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Kalan Limit:";
+            // 
+            // txtLimit
+            // 
+            this.txtLimit.AutoSize = true;
+            this.txtLimit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLimit.Location = new System.Drawing.Point(379, 191);
+            this.txtLimit.Name = "txtLimit";
+            this.txtLimit.Size = new System.Drawing.Size(33, 15);
+            this.txtLimit.TabIndex = 10;
+            this.txtLimit.Text = "5000";
+            this.txtLimit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 446);
+            this.Controls.Add(this.txtLimit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TL);
+            this.Controls.Add(this.dataGridBakiye);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBakiye);
+            this.Controls.Add(this.btnParaCek);
+            this.Controls.Add(this.btnParaYatir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTutar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BankaHesabı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridBakiye)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTutar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnParaYatir;
+        private System.Windows.Forms.Button btnParaCek;
+        private System.Windows.Forms.Label txtBakiye;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridBakiye;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label TL;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label txtLimit;
+    }
+}
+
---
+++ hafta6/BankAccount/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankAccount
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int bakiye = 0;
+        int gunlukMiktar = 0;
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnParaYatir_Click(object sender, EventArgs e)
+        {
+            if (checkInteger(txtTutar.Text))
+            {
+                if (Int32.Parse(txtTutar.Text) > 0)
+                {
+                    bakiye = Int32.Parse(txtBakiye.Text) + Int32.Parse(txtTutar.Text);
+                    gunlukMiktar += Int32.Parse(txtTutar.Text);
+                    txtBakiye.Text = bakiye.ToString();
+                    dataGridBakiye.Rows.Add("Para Yatırıldı", txtTutar.Text + "TL", bakiye + "TL", DateTime.Now);
+                    txtTutar.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("Negatif Değer Giremezsiniz", "Geçersiz İşlem", MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning);
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Girdiğiniz değer integer olmalı", "Yanlış Değer Girdiniz", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+        }
+
+        private void btnParaCek_Click(object sender, EventArgs e)
+        {
+            if (checkInteger(txtTutar.Text))
+            {
+                if (bakiye > 0 && Int32.Parse(txtTutar.Text) <= bakiye)
+                {
+                    if (Int32.Parse(txtLimit.Text) > 0)
+                    {
+                        txtLimit.Text = (Int32.Parse(txtLimit.Text) - Int32.Parse(txtTutar.Text)).ToString();
+
+                        bakiye = Int32.Parse(txtBakiye.Text) - Int32.Parse(txtTutar.Text);
+                        txtBakiye.Text = bakiye.ToString();
+                        dataGridBakiye.Rows.Add("Para Çekildi", txtTutar.Text + "TL", bakiye + "TL", DateTime.Now);
+                        txtTutar.Text = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("limit yetersiz", "Geçersiz İşlem", MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning);
+
+                    }
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Bakiyeniz Yetersiz\nTekrar Deneyiniz!!!", "Geçersiz İşlem", MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Girdiğiniz değer integer olmalı","Yanlış Değer Girdiniz",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        bool checkInteger(string theValue)
+        {
+            return int.TryParse(theValue ,out int n);
+        }
+    }
+}
